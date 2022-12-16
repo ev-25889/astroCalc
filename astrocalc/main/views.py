c763fbee10d820a8c1c52d2745e6ce319d9d8ca8
ev-25889 <106339646+ev-25889@users.noreply.github.com>
2022-12-16 16:22:26 +0500

    Добавила переменные, полученные из форм

--- astrocalc/main/views.py
+++ astrocalc/main/views.py
@@ -5,6 +5,11 @@ from .forms import NameForm
 
 def get_name(request):
     # if this is a POST request we need to process the form data
+    submitbutton = request.POST.get("submit")
+
+    firstname = ''
+    lastname = ''
+    emailvalue = ''
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = NameForm(request.POST)
@@ -13,10 +18,20 @@ def get_name(request):
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+            name = form.cleaned_data.get("your_name")
+            day = form.cleaned_data.get("day")
+            mes = form.cleaned_data.get("mes")
+            #return HttpResponseRedirect('/thanks/')
+
 
     # if a GET (or any other method) we'll create a blank form
     else:
         form = NameForm()
 
-    return render(request, 'main/calc.html', {'form': form})
+
+    context = {'form': form, 'name': name,
+                'day': day, 'submitbutton': submitbutton,
+             'mes': mes}
+
+
+    return render(request, 'main/calc.html', context)
